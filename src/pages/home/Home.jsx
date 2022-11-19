d4b4af2f68de3ef1cddf4441efd58fb221df1516
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2022-11-19 16:18:20 +0530

    esl removed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Navbar from "../../components/navbar/Navbar";
-import NewsLetter from "../../components/newsletter/NewsLetter";
+// import NewsLetter from "../../components/newsletter/NewsLetter";
 import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
     <div className='Home'>
-    <NewsLetter />
     <Navbar />
     <Footer />
     
