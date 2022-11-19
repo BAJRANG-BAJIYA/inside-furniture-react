29a44b6ea0f0888800ae60e1b579356d5135403f
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2022-11-20 01:49:49 +0530

    Banner component created with button

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div className='Banner'>
+    <span className='BannerBtn'>SHOP NOW</span>
+    
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import Navbar from "../../components/navbar/Navbar";
 import NewsLetter from "../../components/newsletter/NewsLetter";
 import Footer from "../../components/footer/Footer";
 import MailList from '../../components/MailList/MailList';
+import Banner from '../../components/banner/Banner';
 
 const Home = () => {
   return (
     <div className='Home'>
     <NewsLetter/>
     <Navbar />
+    <Banner/>
     <MailList/>
     <Footer />
     
