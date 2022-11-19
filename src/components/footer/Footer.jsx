05d546050adf1758b2a11fd1338b7f9a3c8f8575
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2022-11-19 20:59:48 +0530

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,41 +1,42 @@
-import React from 'react'
-import './footer.css'
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <section className='footer'>
-   <p>
-                <span>About</span>
-                <span>Blog</span>
-                <span>Details</span>
-                <span>What we do ? </span>
-                <span>Tokens</span>
-            </p>
-            <p>
-                <span>News</span>
-                <span>Suprises</span>
-                <span>Projects</span>
-                <span>Designs</span>
-                <span>Employers</span>
-            </p>
+    <div className="footer">
+      <div className="fLists">
+        <div className="fList">
+        <div className="footerInfo">ABOUT THE INSIDE</div>
+          <a className="fListItem" href="">About Us</a>
+          <a className="fListItem">Careers</a>
+          <a className="fListItem">COVID-19</a>
+          <a className="fListItem">Press</a>
+          <a className="fListItem">Reviews</a>
+          <a className="fListItem">Blogs</a>
+        </div>
+        <div className="fList">
+        <div className="footerInfo">RESOURCES</div>
+          <a className="fListItem" href="">Contact</a>
+          <a className="fListItem">FAQ</a>
+          <a className="fListItem">Our Guarantee</a>
+          <a className="fListItem">Shipping</a>
+          <a className="fListItem">Financing</a>
+          <a className="fListItem">Affiliates</a>
+          <a className="fListItem">Gift Cards</a>
+        </div>
+        <div className="fList">
+        <div className="footerInfo">TRADE</div>
+          <a className="fListItem" href="">Trade Loyalty Program</a>
+        </div>
+        
+        <div className="fList">
+        <div className="footerInfo">NEED HELP?</div>
+          <a className="fListItem">If you have a question about your order, your swatches, the assortment, or anything else on your mind, we're here for you! </a>
+        
+        </div>
+      </div>
+      <div className="fText">Copyright © 2022 INSIDEFURNITURE.COM.</div>
+    </div>
+  );
+};
 
-            <p>
-                <span>News</span>
-                <span>Suprises</span>
-                <span>Projects</span>
-                <span>Designs</span>
-                <span>Employers</span>
-            </p>
-            <p>
-                <span>News</span>
-                <span>Suprises</span>
-                <span>Projects</span>
-                <span>Designs</span>
-                <span>Employers</span>
-            </p>
-            
-        </section>
-  )
-}
-
-export default Footer
+export default Footer;
