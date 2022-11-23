c76842974983aeb1de0dac181a52d33fa6345572
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2022-11-24 03:42:07 +0530

    newProduct pge created

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './Newproduct.css';
 import Navbar from "../../components/navbar/Navbar";
-import NewsLetter from "../../components/newsletter/NewsLetter";
+ import NewsLetter from "../../components/newsletter/NewsLetter";
 import Footer from "../../components/footer/Footer";
+ 
+ 
 
 
-const Product = () => {
+
+const NewProduct = () => {
   return (
-    <>
-     <NewsLetter />
-    <Navbar />
-<div className='Product'>
-   <h3>Product</h3>
+    <div className='body'>
+    <NewsLetter />
+     <Navbar />
+<div className='productContainer'>
+       <div className="card">
+       <div className="imgBx">
+        <img src='https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVkfGVufDB8fDB8fA%3D%3D&w=1000&q=80' />
+       </div>
+        <div className='content'>
+          <div className='productName'>
+            <h3>rayan ronald</h3>
+          </div>
+          <div className='priceRating'>
+            <h2>13.55</h2>
+            <div className='rating'>
+            {/* <FontAwesomeIcon icon="fa-solid fa-star" />
+            <FontAwesomeIcon icon="fa-solid fa-star" />
+        */}
+            </div>
+          </div>
+          <button>ADD TO CART</button>
+        </div>
+       </div>
     
     </div>
+    
     <Footer />
-
-    </>
+    </div>
     
   )
 }
 
-export default Product
+export default NewProduct;
