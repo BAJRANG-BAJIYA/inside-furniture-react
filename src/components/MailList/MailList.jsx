d66aaff06f2fd4556c0e9d1942b27559214157ba
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2022-12-24 20:47:44 +0530

    maillist css modified

--- src/components/MailList/MailList.jsx
+++ src/components/MailList/MailList.jsx
@@ -4,7 +4,7 @@ const MailList = () => {
   return (
     <div className="mail">
       <h1 className="mail__Title">Save time, save money!</h1>
-      <span className="mail__Desc">Sign up and we'll send the best deals to you</span>
+      <h2 className="mail__Desc">Sign up and we'll send the best deals to you</h2>
       <div className="mail__InputContainer">
         <input type="text" placeholder="Your Email" className="mail__input"/>
         <button className="mail__button">CLICK</button>
