d872e5f0040a48db43de475010f7ccd2520ff4ff
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2022-12-25 20:27:08 +0530

    fontawesome depedancy removed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import "./Navbar.css"
 import { Link }from "react-router-dom"
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 const Navbar = () => {
+  const [isMobile, setIsMobile]=useState(false)
   return (
     <div className="navbar">
-      <ul className="leftSide">
+      <ul className= { isMobile ? "nav-links-mobile" : "leftSide"}>
         <li className="leftContent"><Link to ="/newproduct" style={{textDecoration:'none'}}><span >NEW</span></Link></li>
         <li className="leftContent"><Link to ="/bedroom" style={{textDecoration:'none'}}><span>BEDROOM</span></Link></li>
         <li className="leftContent"><Link to ="/living" style={{textDecoration:'none'}}><span>LIVING</span></Link></li>
@@ -14,15 +18,23 @@ const Navbar = () => {
 <a  aria-label="The Inside" href="/" className="logo"><Link to ="/"><img className="insideLogo" src="https://theinside.imgix.net/nav/logo.svg?format=auto" alt="logo"></img></Link>
   
 </a>
-      <ul className="rightSide">
+      <ul className= { isMobile ? "nav-links-mobile" : "rightSide"}>
       <li className="rightContent"><span>COLLABORATION</span></li>
       <li className="rightContent"><span>DESIGNS</span></li>
       <li className="rightContent"><Link to ="/register" style={{textDecoration:'none'}}><span>REGISTER</span></Link></li>
       <li className="rightContent"><Link to ="/login" style={{textDecoration:'none'}}><span>LOGIN</span></Link></li>
       <li className="rightContent"><Link to ="/search" style={{textDecoration:'none'}}><span>SEARCH</span></Link></li> 
-      <li className="rightContent"><Link to ="/Cart" style={{textDecoration:'none'}}><span>CART</span></Link></li>
+      <li className="rightContent"><Link to ="/search" style={{textDecoration:'none'}}><span>CART</span></Link></li>
           
         </ul>
+        <button className="mobile-menu-icon">
+          {isMobile ? (
+            <i className="FaBars"><FaBars /></i>
+          ) : (
+            <i className="FaTimes"><FaTimes /></i>
+          )
+          }
+        </button>
     </div>
   )
 }
