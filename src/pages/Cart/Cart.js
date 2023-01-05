cf9c94037188dde6c86484e0922d31b263c32ce4
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-05 23:14:06 +0530

    cart html added

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -4,9 +4,22 @@ import "./Cart.css"
 const Cart = () => {
   return (
     <>
-  <div className="Cart">
-<div className='Cart__Container'>
- Cart
+  <div className="CartContainer">
+  <h1>Shopping Cart</h1>
+<div className='CartContainer__Cart'>
+    <div className='CartContainer__Cart__Products'>
+      <div className='CartContainer__Cart__Products__Product'>
+        <img  src=''/>
+        <div className='CartContainer__Cart__Products__Product-info'>
+          <h3 className='CartContainer__Cart__Products__Product-name'>New Bed</h3>
+          <h2 className='CartContainer__Cart__Products__Product-price'>$1100</h2>
+          <h2 className='CartContainer__Cart__Products__Product-offer'>20%</h2>
+          <p className='CartContainer__Cart__Products__Product-quantity'>Qnt:2</p> <input value="1" name=''/>
+          
+        </div>
+      </div>
+    </div>
+ 
 </div>
   </div>
     </>
