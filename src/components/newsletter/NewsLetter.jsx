3540f4cff6a67d3c47c0b64ae2f3754570503025
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-19 17:30:37 +0530

    newsletter css responsiveness added

--- src/components/newsletter/NewsLetter.jsx
+++ src/components/newsletter/NewsLetter.jsx
@@ -4,7 +4,7 @@ import './newsletter.css'
 const NewsLetter = () => {
   return (
     <div className='NewsLetter'>
-    <h4 className='NewsContent'>EARLY ACCESS TO OUR BIGGEST SALE OF THE YEAR! SAVE UP TO <strong>30%</strong> WITH CODES <strong>BF20</strong>, BF25, AND BF30</h4>
+    <h4 className='NewsLetter__NewsContent'>EARLY ACCESS TO OUR BIGGEST SALE OF THE YEAR! SAVE UP TO <strong>30%</strong> WITH CODES <strong>BF20</strong>, BF25, AND BF30</h4>
     </div>
   )
 }
