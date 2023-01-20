7a92b25d27391b093d6967011be08c2f0708d519
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-20 23:01:59 +0530

    cart modified

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,29 +1,44 @@
 import React from 'react'
 import "./Cart.css"
-import { BiTrash } from "react-icons/bi";
+import { BiCartAlt, BiTrash } from "react-icons/bi";
+
+import Navbar from "../../components/navbar/Navbar";
 
 const Cart = () => {
   return (
     <>
-  <div className="CartContainer">
-  <h1 className='CartContainer__CartHeading'>Shopping Cart</h1>
-<div className='CartContainer__Cart'>
-    <div className='CartContainer__Cart__Products'>
-      <div className='CartContainer__Cart__Products__Product'>
+    <Navbar/>
+  <div className="Wrapper">
+  <h1>Shopping Cart</h1>
+<div className='project'>
+    <div className='shop'>
+      <div className='box'>
         <img  src='https://images.unsplash.com/photo-1672840087930-9717fd4f5609?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'/>
-        <div className='CartContainer__Cart__Products__Product-info'>
-          <h3 className='CartContainer__Cart__Products__Product-name'>New Bed</h3>
-          <h2 className='CartContainer__Cart__Products__Product-price'>$1100</h2>
-          <h2 className='CartContainer__Cart__Products__Product-offer'>20%</h2>
-          <p className='CartContainer__Cart__Products__Product-quantity'>Qnt:2</p> <input value="1" name=''/>
-          <p>
-            <i className='' aria-hidden="true"><BiTrash/></i>
-            <span className='CartContainer__Cart__Products__Product-remove'>Remove</span>
+        <div className='content'>
+          <h3 >New Bed</h3>
+          <h4 className='price'>Price:$1100</h4>
+ 
+          <p className='unit'>Quantity: <input value="1" name=''/></p>
+          <p className='btn-area'>
+            <i className='trash' ><BiTrash/></i>
+            <span className='btn2'>Remove</span>
           </p>
         </div>
       </div>
+      
     </div>
-    <div className='cart-total'></div>
+          <div className='right-bar'>
+            <p><span>Subtotal</span> <span>$1200</span></p>
+            <hr/>
+            <p><span>Tax(5%):</span> <span>$72</span></p>
+            <hr/>
+            <p><span>Shopping</span> <span>$1272</span></p>
+            <hr/>
+            <p><span>Total:</span> <span>$1272</span></p>
+
+            <a href='#'><i className='shoping-cart'><BiCartAlt /></i>CHECKOUT</a>
+            <hr/>
+          </div>
 </div>
   </div>
     </>
