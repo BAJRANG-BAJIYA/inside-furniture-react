eb00d27fe824f84baca7c93e8cc8d16c6af7c9cd
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-21 14:39:02 +0530

    login css modified

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from "../../components/navbar/Navbar";
 import NewsLetter from "../../components/newsletter/NewsLetter";
-import Footer from "../../components/footer/Footer";
 import { Link } from 'react-router-dom';
 import "./login.css";
 
@@ -23,7 +22,6 @@ const Login = () => {
    </div>
     
     </div>
-    <Footer />
     </>
     
   )
