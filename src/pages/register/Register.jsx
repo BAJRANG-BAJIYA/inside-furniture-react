7aacb8499c7a856e745ead387a9619be788adca0
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-21 18:42:13 +0530

    register page modified

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import "./register.css"
 import Navbar from "../../components/navbar/Navbar";
 import NewsLetter from "../../components/newsletter/NewsLetter";
-import Footer from "../../components/footer/Footer";
-
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
@@ -15,24 +14,21 @@ const Register = () => {
     
     <form className='Register__Form'>
     <h1 className='Register__Title'>CREATE AN ACCOUNT</h1> 
-      <input placeholder='FirstName' className="Register__registerInput" />
-      <input placeholder='LastName' className="Register__registerInput" />
-      <input placeholder='UserName' className="Register__registerInput" />
-      <input placeholder='PassWord' className="Register__registerInput" />
+      <input placeholder='Firstname' className="Register__registerInput" />
+      <input placeholder='Lastname' className="Register__registerInput" />
+      <input placeholder='Username' className="Register__registerInput" />
+      <input placeholder='Password' className="Register__registerInput" />
       <input placeholder='Confirm Password' className="Register__registerInput" />
-      <span className='Register__Agreement'>By Creating an account, I consent to the processig of my personal data in accordance with the <b>PRIVACY POLICY</b>
+      <span className='Register__Agreement'>By Creating an account, I consent to the processig of my data in with the <b>PRIVACY POLICY</b>
       </span>
-      <span className="Register__formButton">CREATE</span>
+      <Link to ="/login" style={{textDecoration:'none'}}>
+      <button className="Register__Button">CREATE</button></Link>
     </form>
 
    </div>
-    
+     
     </div>
-    <Footer />
-
-    </>
-    
+    </>   
   )
 }
-
 export default Register
