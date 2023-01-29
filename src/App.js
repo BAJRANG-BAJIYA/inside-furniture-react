36c265f838e85e4ed222127209d9f67d050cce95
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-30 00:42:05 +0530

    lazy loading added to office dining bedroom page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
 import React from "react";
-import Home from "./pages/home/Home";
-import Office from "./pages/Office/Office";
-import Dining from "./pages/Dining/Dining";
-import Bedroom from "./pages/Bedroom/Bedroom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/register/Register";
 import Living from "./pages/Living/Living";
 import Outdoor from "./pages/Outdoor/Outdoor";
 import Cart from "./pages/Cart/Cart";
 import { Routes, Route} from "react-router-dom";
+const LazyHome = React.lazy(()=>import('./pages/home/Home'));
 const LazySearch = React.lazy(()=>import('./pages/Search/Search'));
 const LazyNewProduct = React.lazy(()=>import('./pages/NewProduct/NewProduct'));
+const LazyOffice = React.lazy(()=>import('./pages/Office/Office')); 
+const LazyDining = React.lazy(()=>import('./pages/Dining/Dining')); 
+const LazyBedroom = React.lazy(()=>import('./pages/Bedroom/Bedroom')); 
 
 function App() {
   return (
     <div className="App">
   <Routes>
-  <Route exact path="/" element={<Home/>} />
+  <Route 
+  exact path="/" 
+  element={
+    <React.Suspense fallback='Loading'>
+        <LazyHome/>
+    </React.Suspense>
+  } />
   
     <Route 
     path ="/newproduct" 
     element={
       <React.Suspense fallback='Loading...'>
-        <NewProduct/>
+        <LazyNewProduct/>
       </React.Suspense>
     }/>
-    <Route path ="/office" element={<Office/>}/>
-    <Route path ="/dining" element={<Dining/>}/>
-    <Route path ="/bedroom" element={<Bedroom/>}/>
+    <Route 
+    path ="/office" 
+    element={
+      <React.Suspense fallback='Loading...'>
+        <LazyOffice/>
+      </React.Suspense>
+    }/>
+
+    <Route 
+    path ="/dining" 
+    element={
+      <React.Suspense fallback='Loading'>
+        <LazyDining/>
+      </React.Suspense>
+    }/>
+
+    <Route 
+    path ="/bedroom" 
+    element={
+      <React.Suspense fallback='Loading'>
+        <LazyBedroom/>
+      </React.Suspense>
+    }/>
+    
     <Route path ="/living" element={<Living/>}/>
     <Route path ="/outdoor" element={<Outdoor/>}/>
     <Route 
