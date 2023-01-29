9a9ea62de1dbffcbeb9c3d5d661a7bea85cb4233
BAJRANG-BAJIYA <90967755+BAJRANG-BAJIYA@users.noreply.github.com>
2023-01-29 23:09:16 +0530

    lazyloading added to the newproduct page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Dining from "./pages/Dining/Dining";
 import Bedroom from "./pages/Bedroom/Bedroom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/register/Register";
-import NewProduct from "./pages/NewProduct/NewProduct";
 import Living from "./pages/Living/Living";
 import Outdoor from "./pages/Outdoor/Outdoor";
 import Cart from "./pages/Cart/Cart";
 import { Routes, Route} from "react-router-dom";
 const LazySearch = React.lazy(()=>import('./pages/Search/Search'));
+const LazyNewProduct = React.lazy(()=>import('./pages/NewProduct/NewProduct'));
 
 function App() {
   return (
@@ -18,7 +18,13 @@ function App() {
   <Routes>
   <Route exact path="/" element={<Home/>} />
   
-    <Route path ="/newproduct" element={<NewProduct/>}/>
+    <Route 
+    path ="/newproduct" 
+    element={
+      <React.Suspense fallback='Loading...'>
+        <NewProduct/>
+      </React.Suspense>
+    }/>
     <Route path ="/office" element={<Office/>}/>
     <Route path ="/dining" element={<Dining/>}/>
     <Route path ="/bedroom" element={<Bedroom/>}/>
